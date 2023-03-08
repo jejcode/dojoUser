fe45a364615aafc0bbcfe3b78b8a042df6f5b084
Joel Jacobson <joel.e.jacobson@gmail.com>
2023-03-08 09:43:39 -0700

    updated code so that methods can be chained

--- user.py
+++ user.py
@@ -9,9 +9,11 @@ class User:
         self.gold_card_points = 0
     
     def display_info(self):
+        print('-----------------')
         for key,value in vars(self).items():
             print(f"{key}: {value}")
         print('-----------------')
+        return self
     
     def enroll(self):
         if self.is_rewards_member == False:
@@ -19,30 +21,23 @@ class User:
             self.gold_card_points = 200
         else:
             print(f"{self.first_name}, you are already a member.")
+        return self
     
     def spend_points(self, amount):
         if amount < self.gold_card_points:
             self.gold_card_points -= amount
         else:
             print(f"{self.first_name}, you don't have that many points to spend.")
+        return self
 
 joel = User('Joel', 'Jacobson', 'joel@codingisthebestever.com', 21)
-joel.display_info()
-joel.enroll()
 joel.display_info()
 
 aslan = User('Aslan','','aslan@narnia.com', 457)
 mr_tumnus = User('Mr', 'Tumnus', 'mrTum@narnia.com', 25)
 lucy = User('Lucy', "Pevensie", 'lucy@narnia.com', 8)
 
-joel.spend_points(50)
-aslan.enroll()
-aslan.spend_points(80)
-
-joel.display_info()
-aslan.display_info()
-mr_tumnus.display_info()
+joel.enroll().spend_points(50).display_info()
+aslan.enroll().spend_points(80).display_info().enroll()
+mr_tumnus.display_info().spend_points(40)
 lucy.display_info()
-
-mr_tumnus.spend_points(40)
-aslan.enroll()
